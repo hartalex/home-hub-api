92e36218abb443ae10cba361b46b35480af55851
Alex Hart <alex@hartcode.com>
2019-02-26 19:46:08 -0600

    feat(temp_current.js): Fix api hub url

--- src/server/api/routes/temperatures/temp_current.js
+++ src/server/api/routes/temperatures/temp_current.js
@@ -7,26 +7,36 @@ const slack = require('../../data/slack')(config.slackUrl)
 const errorHandler = require('../errorHandler')(slack)
 
 module.exports = function (req, res, done) {
-  fetch('http://hub.hartcode.com/temp/sensor/list')
-  .then(jsonResponseHandler)
-  .then(function (sensorjson) {
-    var retval = []
-    var dbobj = req.db
-    Promise.all(sensorjson.data.map(function (sensor) {
-      return new Promise(function (resolve, reject) {
-        db.queryLastData(dbobj, {sensorId: sensor.sensorId}, {utc_timestamp: -1}, 'temperatures', function (error, temp) {
-          if (error) { throw error }
-          if (temp != null) {
-            temp.sensorName = sensor.name
-            temp.outdated = new Date() - new Date(temp.utc_timestamp) > 5 * 60000
-            delete temp._id
-            retval.push(temp)
-          }
-          resolve()
+  fetch('http://api.hub.hartcode.com/temp/sensor/list')
+    .then(jsonResponseHandler)
+    .then(function (sensorjson) {
+      var retval = []
+      var dbobj = req.db
+      Promise.all(
+        sensorjson.data.map(function (sensor) {
+          return new Promise(function (resolve, reject) {
+            db.queryLastData(dbobj, { sensorId: sensor.sensorId }, { utc_timestamp: -1 }, 'temperatures', function (
+              error,
+              temp
+            ) {
+              if (error) {
+                throw error
+              }
+              if (temp != null) {
+                temp.sensorName = sensor.name
+                temp.outdated = new Date() - new Date(temp.utc_timestamp) > 5 * 60000
+                delete temp._id
+                retval.push(temp)
+              }
+              resolve()
+            })
+          })
         })
-      })
-    })).then(function () {
-      res.json(retval)
-    }).catch(errorHandler(req, res, done))
-  }).catch(errorHandler(req, res, done))
+      )
+        .then(function () {
+          res.json(retval)
+        })
+        .catch(errorHandler(req, res, done))
+    })
+    .catch(errorHandler(req, res, done))
 }
