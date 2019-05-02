9c92f7c73bb8ae8f0ff56d1fe142fac4d2aafda0
Alex Hart <alex@hartcode.com>
2019-05-01 22:18:12 -0500

    fix(temp_current): revert url config change as it doesn't work

--- src/server/api/routes/temperatures/temp_current.js
+++ src/server/api/routes/temperatures/temp_current.js
@@ -7,7 +7,7 @@ const slack = require('../../data/slack')(config.slackUrl)
 const errorHandler = require('../errorHandler')(slack)
 
 module.exports = function (req, res, done) {
-  fetch(config.url + '/temp/sensor/list')
+  fetch('http://api.hub.hartcode.com/temp/sensor/list')
     .then(jsonResponseHandler)
     .then(function (sensorjson) {
       var retval = []
