cab47834b4d0846bf88055343146319f6708d348
Alex Hart <alex@hartcode.com>
2019-05-01 20:28:15 -0500

    feat(mongodb): Try adding replicaset to the connection url:

--- src/server/api/db/url.js
+++ src/server/api/db/url.js
@@ -1,2 +1,2 @@
 // Connection URL
-module.exports = 'mongodb://mongo-0.mongodb2,mongo-1.mongodb2,mongo-2.mongodb2:27017'
+module.exports = 'mongodb://mongo-0.mongodb2,mongo-1.mongodb2,mongo-2.mongodb2:27017/temphub?replicaSet=rs0'
